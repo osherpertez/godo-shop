6871c5b0bb5e1668c2e662d3293639a7427a404e
https://github.com/team <osherpertez@gmail.com>
2021-04-15 23:31:11 +0300

    initial comit

---
+++ src/Products.js
@@ -0,0 +1,21 @@
+import './Products.css';
+import Product from './Product'
+
+function Products(){
+    return(
+        <section className="products">
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>     
+        </section>
+    );
+}
+
+export default Products;
